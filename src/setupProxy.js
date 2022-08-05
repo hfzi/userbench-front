3b017798ff802fc945483259d25f7ac7ecad10fb
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-08-05 13:04:04 +0300

    updated http-proxy-middleware

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+module.exports = app => {
+    app.use(createProxyMiddleware('/auth', {
+      target: "https://userbench-back.vercel.app",
+      changeOrigin: true
+    })),
+    app.use(createProxyMiddleware('/auth/login/confirm', {
+      target: "https://userbench-back.vercel.app",
+      changeOrigin: true
+    }))
+  }
