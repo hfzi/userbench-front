257d30e8ed51bbec04d00d83dc90ed1b10f8c247
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-08-05 12:00:01 +0300

    updated date-fns

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Product from "./pages/Product";
 import AddProduct from "./pages/Product/Add";
 import Profile from "./pages/Profile";
 import Navbar from "./pages/Navbar";
-//import "./App.css";
+import "date-fns";
 
 function App() {
 	const [user, setUser] = useState(null);
