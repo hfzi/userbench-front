1394110bdbf26bd8cdbc344a846f7a730caf78f3
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-09-05 13:50:58 +0300

    updated main page

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,13 +1,59 @@
-// import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function Product(userDetails) {
-	const user =  userDetails.user ? userDetails.user.data : ""
-	console.log("user", user)
-	return (
-		<div>
-			sa {user.name && user.name}
-		</div>
-	);
+function Product() {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    const user = async () => {
+      const { data } = await axios.get(process.env.REACT_APP_HOST + "/search");
+      setUser(data);
+    };
+    user();
+  }, []);
+
+  console.log("veri", users ? users : "");
+
+
+  return (
+    <div className="container" style={{ backgroundColor: "#DADADA" }}>
+      <div className="row">
+        <table>
+          {users &&
+            users.map((x, i) => {
+              return (
+              <>
+                <tr>
+                  <th>
+                    <div key={i}>
+                    <img
+                        style={{
+                          maxWidth: "222px",
+                          height: "222px",
+                          borderRadius: "8px",
+                          objectFit: "contain",
+                          display: "block",
+                          marginLeft: "auto",
+                          marginRight: "auto",
+                          zIndex: "2",
+                        }}
+                        src={x.image}
+                        alt={i}
+                      />
+                    </div>
+                  </th>
+                  <th>{x.name}</th>
+                  <th>{x.BoostClock}</th>
+                  <th>{x.MemorySize}</th>
+                  <th>{x.TDP}</th>
+                </tr>
+              </>
+              )
+})}
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
